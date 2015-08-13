15c2f1b463d72b396c8386e8fd638a3f49d4c225
Jason Larke <jasonlarke@live.com.au>
2015-08-13 16:54:01 +0800

    Shifted 'di' components into a separate folder, renamed minErr to mkerr

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 "use strict";
 
 var diModule = require("./lib/di-module"),
-    injector = require("./lib/injector"),
-    ndi      = require("./lib/core")(diModule);
+    injector = require("./lib/injector");
+
+// Initialize all the core 'di' functionality    
+require("./lib/di/core")(diModule);
     
 module.exports = {
     module:     diModule,
--- test.js
+++ test.js
@@ -1,22 +1,10 @@
-var di = require("./index");
+var di = require("./index"),
+    $RouteProvider = require("./route");
 
 var app = di.module("app", []);
 
-app.constant("DepType", {
-	UNKNOWN: 0,
-	CIRCULAR: 1,
-	SIMPLE: 2
-});
+app.provider("$route", $RouteProvider);
 
-app.factory("$httpq", [ "DepType", function(DepType) {
-    return {
-        log: function(x) {
-            console.log("$httpq: " + DepType.SIMPLE);
-        }
-    };
-}]);
-
-
-var injector = di.injector([ "app" ]).invoke(["$httpq", function($httpq) {
-    $httpq.log("test");
+di.injector([ "app" ]).invoke(["$route", function($route) {
+    console.log("here");
 }]);
