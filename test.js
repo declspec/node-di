97708a10187922230aacb6dce8deca235a43a956
Jason Larke <jasonlarke@live.com.au>
2015-08-14 11:49:48 +0000

    Renamed project and remove unfinished  module from the included core

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
 var di = require("./index");
-
-di.injector(["di"]).invoke([ "$controller", "$http", function($controller, $http) {
-    $http.get("http://serverfault.com/questions/674974/how-to-mount-a-virtualbox-shared-folder").then(function(obj) {
-        console.log(obj);
-    });
-}]);
